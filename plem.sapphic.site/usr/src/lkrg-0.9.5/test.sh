4d1df74762d6fd3275d65a698fdcf92bc05fbbca
r3g_5z <june@girlboss.ceo>
2022-12-31 15:42:38 -0500

    actually update plem.sapphic.site server configs

--- plem.sapphic.site/usr/src/lkrg-0.9.5/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eux
-# This script is used by out-of-tree (a tool to assist kernel module testing),
-# https://github.com/jollheef/out-of-tree
-dmesg | grep 'LKRG initialized successfully!'
